3b835ca633f5ea3d84de02106fb28ad230d8c034
Shawn Chen <shawnchendev@users.noreply.github.com>
2018-05-03 13:42:05 -0230

    add position and safe area view for iPhone X

--- MessageBar.js
+++ MessageBar.js
@@ -14,26 +14,22 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 0,
     right: 0,
-    top: 0
   },
-
   message: {
     paddingVertical: 15,
     paddingHorizontal: 20,
     justifyContent: 'center',
-    backgroundColor: 'grey'
+    backgroundColor: 'grey',
   },
   messageText: {
     color: 'white',
-    fontSize: 18
   },
 });
 
-function Message({ message, messageContainer, messageText }) {
+function Message({ message }) {
   return (
-
-    <View style={[styles.message, messageContainer]}>
-      <Text style={messageText || styles.messageText}>{message}</Text>
+    <View style={styles.message}>
+      <Text style={styles.messageText}>{message}</Text>
     </View>
   );
 }
@@ -99,29 +95,28 @@ export default class MessageBar extends Component {
     }
   }
   render() {
-    let { messageComponent: MessageComponent, slideAnimationOffset, messageContainer, messageText } = this.getConfig();
-
+    let { messageComponent: MessageComponent, slideAnimationOffset, position } = this.getConfig();
+    if (position.hasOwnProperty('top')) { slideAnimationOffset = 0 - slideAnimationOffset }
     const translateY = this.state.isVisibleAnimValue.interpolate({
       inputRange: [0, 1],
       outputRange: [slideAnimationOffset, 0],
-
     });
     const opacity = this.state.isVisibleAnimValue.interpolate({
       inputRange: [0, 1],
       outputRange: [0, 1],
     });
-    console.log(this.getConfig())
     return (
       <Animated.View
         style={[
           styles.root,
+          position,
           { transform: [{ translateY }] },
           { opacity },
         ]}
       >
         <SafeAreaView {...this.panResponder.panHandlers}>
           {this.state.message &&
-            <MessageComponent message={this.state.message.message} messageContainer={messageContainer} messageText={messageText} />
+            <MessageComponent message={this.state.message.message} />
           }
         </SafeAreaView>
       </Animated.View>
@@ -132,11 +127,9 @@ export default class MessageBar extends Component {
 MessageBar.defaultProps = {
   messageComponent: Message,
   duration: 1000,
-  slideAnimationOffset: -40,
+  slideAnimationOffset: 40,
   showAnimationDuration: 255,
   hideAnimationDuration: 255,
   closeOnSwipe: true,
-  position: 'top',
-  messageContainer: null,
-  messageText: null,
+  position: { top: 0 }
 };
